7979b87db7f15a5c21ce41e8089fee64ee250b01
Madeline Urbanic <madelinerurbanic@gmail.com>
2024-01-24 21:42:06 -0500

    base

---
+++ src/pages/contact.jsx
@@ -0,0 +1,11 @@
+export default function contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+                <h2>About Me</h2>
+                <p>
+                    TBD
+                </p>
+        </div>
+    );
+}
---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,10 @@
+export default function portfolio() {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+                <p>
+                    TBD
+                </p>
+        </div>
+    );
+}
