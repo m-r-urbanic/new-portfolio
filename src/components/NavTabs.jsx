94c15ed84e96452f98ded09577ae8fa5c14cfd35
Madeline Urbanic <madelinerurbanic@gmail.com>
2024-02-06 15:34:08 -0500

    basic contact

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,7 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 function NavTabs() {
   const currentPage = useLocation().pathname;
 
@@ -10,8 +8,6 @@ function NavTabs() {
       <li className="nav-item">
         <Link
           to="/"
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
           className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
         >
           About
@@ -20,7 +16,6 @@ function NavTabs() {
       <li className="nav-item">
         <Link
           to="/Resume"
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
         >
           Resume
@@ -29,7 +24,6 @@ function NavTabs() {
       <li className="nav-item">
         <Link
           to="/Portfolio"
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
         >
           Portfolio
@@ -38,7 +32,6 @@ function NavTabs() {
       <li className="nav-item">
         <Link
           to="/Contact"
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
         >
           Contact
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,10 +1,71 @@
-export default function Contact() {
+import { useState } from 'react';
+
+function Contact() {
+  const [Name, setName] = useState('');
+  const [Email, setEmail] = useState('');
+  const [Message, setMessage] = useState('');
+
+  const handleInputChange = (e) => {
+
+    const { name, value } = e.target;
+
+    switch (name) {
+      case 'Name':
+        setName(value);
+        break;
+      case 'Email':
+        setEmail(value);
+        break;
+      case 'Message':
+        setMessage(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+
+    e.preventDefault();
+    alert(`Hello ${Name} ${Email} ${Message}`);
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Insert Form Here
-      </p>
+    <div className="container text-center">
+      <h1>
+        TEST {Name} {Email} {Message}
+      </h1>
+      <form className="form" onSubmit={handleFormSubmit}>
+        <input
+          value={Name}
+          name="Name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          value={Email}
+          name="Email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          value={Message}
+          name="Message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Message"
+        />
+        <button type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
+
+export default Contact;
